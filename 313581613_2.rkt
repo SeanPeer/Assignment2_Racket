1f5b4d4103ed33ddb9a1a1950083b5348799c0cc
Sean Peer <61992205+SeanPeer@users.noreply.github.com>
2022-12-15 18:11:31 +0200

    Add files via upload

--- 313581613_2.rkt
+++ 313581613_2.rkt
@@ -51,6 +51,7 @@ SE=3>
 
 
 #|
+3A
 TODO ADD COMMENTS!!!
 |#
 
@@ -115,20 +116,26 @@ TODO ADD COMMENTS!!!
       [(list '+ lhs rhs) (Add (parse-sexpr lhs) (parse-sexpr rhs))] 
       [(list '- lhs rhs) (Sub (parse-sexpr lhs) (parse-sexpr rhs))] 
       [(list '* lhs rhs) (Mul (parse-sexpr lhs) (parse-sexpr rhs))] 
-      [(list '/ lhs rhs) (Div (parse-sexpr lhs) (parse-sexpr rhs))] 
+      [(list '/ lhs rhs) (Div (parse-sexpr lhs) (parse-sexpr rhs))]
+      
  [else (error 'parse-sexpr "bad syntax in ~s" sexpr)])) 
-   
+
+#|
+  3B
+  |#
   (: parse : String -> PLANG) 
   ;; parses a string containing a PLANG expression to a PLANG AST
 (define (parse str) 
     (let ([code (string->sexpr str)]) 
       (match code
-        [(list (list 'poly a...) (list d...)) (Poly (parse-list(list 'poly a...))
-                                              (parse-list (list d...)))]
-        ;[(list (list 'poly a b) (list d e f)) (Poly (parse-list(list a b c)) (parse-list(list d e f)))]
-        [(list (list 'poly) (list d e )) (error 'parse "at least one coefficient is required in ~s" code)]
-        [(list (list 'poly a b )(list)) (error 'parse "at least one point is required in ~s" code)])))
-
+       ;;[(list lhs rhs) (Poly (parse-list(list lhs))(parse-list(list rhs)))]
+       ;;[(list (first(list code))  (second code)) (Poly  (parse-list(list(first code)))
+       ;;                                           (parse-list (list (list(second code)))))]
+        [(list (list 'poly ) (list r ...)) (error 'parse "at least one coefficient is required in ~s" code)]
+        [(list (list 'poly l ...) (list)) (error 'parse "at least one point is required in ~s" code)]
+        [(list (list 'poly l ...) (list r ...)) (Poly (parse-list l)
+                                                      (parse-list r))]
+        [else (error 'parse "Another unknown error is occured in ~s" code)])))
 
 (: parse-list : (Listof Sexpr) -> (Listof AE))
 (define (parse-list listxpr)
@@ -142,10 +149,35 @@ TODO ADD COMMENTS!!!
 
 (test (parse "{{poly 1 2 3} {1 2 3}}")  
      => (Poly (list (Num 1) (Num 2) (Num 3))  
-              (list (Num 1) (Num 2) (Num 3)))) 
-(test (parse "{{poly } {1 2} }")   =error> "parse: at least one coefficient is required in ((poly) (1 2))") 
+              (list (Num 1) (Num 2) (Num 3))))
+(test (parse "{{poly 1 2 3 4} {1 2 3 4}}")  
+     => (Poly (list (Num 1) (Num 2) (Num 3) (Num 4))  
+              (list (Num 1) (Num 2) (Num 3) (Num 4)))) 
+(test (parse "{{poly} {1 2} }")   =error> "parse: at least one coefficient is required in ((poly) (1 2))") 
 (test (parse "{{poly 1 2 } {} }")       =error> "parse: at least one point is required in ((poly 1 2) ())") 
 
 (test (parse "{{poly 1 2 } {1 2 }}")  
      => (Poly (list (Num 1) (Num 2))  
               (list (Num 1) (Num 2)))) 
+
+;; 3C
+;; evaluates AE expressions to numbers
+(: eval : AE -> Number)
+  (define (eval expr) 
+    (cases expr 
+      [(Num n)  n] 
+      [(Add l r) (+ (eval l) (eval r))] 
+      [(Sub l r) (- (eval l) (eval r))] 
+      [(Mul l r) (* (eval l) (eval r))] 
+      [(Div l r) (/ (eval l) (eval r))]))
+
+
+  (: eval-poly : PLANG -> (Listof Number) ) 
+  (define (eval-poly p-expr) 
+    (cases p-expr
+      [(Poly f s)(map(createPolynomial(map eval f)) (map eval s))])) 
+ 
+  (: run : String -> (Listof Number)) 
+  ;; evaluate a FLANG program contained in a string 
+  (define (run str) 
+    (eval-poly (parse str))) 
